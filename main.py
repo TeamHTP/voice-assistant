e5d5eccd02ca9a323938c172669c13cbddb5f18b
Tianyi Ma <tn.ma@outlook.com>
2019-11-09 12:34:41 -0600

    stt with callback in main

--- main.py
+++ main.py
@@ -1,8 +1,36 @@
-import src.natural_language_processing as natty
+from src import natural_language_processing as natty
+from src import speech_to_text as stt
+from types import SimpleNamespace
+import argparse
 
 
 def main():
+    parser = argparse.ArgumentParser(description='Stream from microphone using VAD')
+    parser.add_argument('-m', '--model', required=True,
+                        help='Path to the model (protocol buffer binary file, or entire directory containing all standard-named files for model)')
+    args = parser.parse_args()
+    stt_settings = {
+      'alphabet': 'alphabet.txt',
+      'beam_width': 500,
+      'device': None,
+      'lm': 'lm.binary',
+      'lm_alpha': 0.75,
+      'lm_beta': 1.85,
+      'model': args.model,
+      'n_context': 9,
+      'n_features': 26,
+      'nospinner': False,
+      'rate': 16000,
+      'savewav': None,
+      'trie': 'trie',
+      'vad_aggressiveness': 3
+    }
+    stt.init(SimpleNamespace(**stt_settings), on_speech)
     natty.text_to_token("Hey Oracle, what time is it")
+
+
+def on_speech(text):
+    print("Recognized: %s" % text)
 
 
 if __name__ == '__main__':
