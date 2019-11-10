be0bbc07a38b19cb5f7f04c0199edb43935f5f68
dominictassio <dominic.tassio@gmail.com>
2019-11-10 01:51:43 -0600

    add hello world skill

---
+++ src/skills/hello.py
@@ -0,0 +1,17 @@
+from src import text_to_speech as tts
+from src.skills.skills import register
+
+
+@register
+class Hello:
+
+    primary_triggers = ['hello']
+
+    def do(self, params):
+        tts.say('Hello, world!');
+
+    def get_confidence(self, synonyms, spoken):
+        if 'hello' in spoken:
+            return 1
+        else:
+            return 0
